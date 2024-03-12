e9eacecc822574d54cba5f74381534e52fe248e8
Aaron O'Mullan <aaron.omullan@gmail.com>
2024-03-13 00:34:28 +0200

    tweak: qtrpc => q prefix aliases
    qtrpc can be a mouthful

--- example/worker.ts
+++ example/worker.ts
@@ -1,26 +1,24 @@
-import { qtrpcClient, qtrpcHandler, QTRPCMessage } from "../src/index";
+import { qClient, qHandler, QMessage } from "../src/index"; // "qtrpc"
 import { type AppRouter, appRouter } from "./router";
 import type { Queue, MessageBatch } from "@cloudflare/workers-types";
 
 interface Env {
-  QUEUE: Queue<QTRPCMessage>;
+  QUEUE: Queue<QMessage>;
 }
-
-const qtrpcHandle = qtrpcHandler(appRouter);
 
 export default {
   async fetch(request: Request, env: any): Promise<Response> {
-    const qtrpc = qtrpcClient<AppRouter>(env.QUEUE);
-    await qtrpc.projects.checkUsage.send({ projectId: "" });
-    await qtrpc.users.sendEmail.send({ email: "john@gmail.com" });
-    await qtrpc.users.sendEmail.sendBatch([
+    const q = qClient<AppRouter>(env.QUEUE);
+    await q.projects.checkUsage.send({ projectId: "" });
+    await q.users.sendEmail.send({ email: "john@gmail.com" });
+    await q.users.sendEmail.sendBatch([
       { email: "john1@gmail.com" },
       { email: "john2@gmail.com" },
     ]);
-    await qtrpc.users.boom.send();
+    await q.users.boom.send();
     return new Response("Success");
   },
-  async queue(batch: MessageBatch<QTRPCMessage>, env: Env): Promise<void> {
-    await qtrpcHandle(batch, { env });
+  async queue(batch: MessageBatch<QMessage>, env: Env): Promise<void> {
+    await qHandler(appRouter)(batch, { env });
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export * from "./client";
 export * from "./server";
 export * from "./types";
+
+// Aliases qtrpc => q
+export { qtrpcHandler as qHandler } from "./server";
+export { qtrpcClient as qClient } from "./client";
+export { QTRPCMessage as QMessage } from "./types";
